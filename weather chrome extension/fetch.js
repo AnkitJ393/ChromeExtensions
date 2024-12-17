d5a1b9f7fe9e45844a214e92219272a090d5fbee
AnkitJ393 <jangidankit767@gmail.com>
2024-12-18 00:27:43 +0530

    refactored the code

--- weather chrome extension/fetch.js
+++ weather chrome extension/fetch.js
@@ -1,33 +1,57 @@
 const API_key='77951553f1fa58b2de881ba6c880af03';
 
 window.onload=()=>{
-    const geoSuccess=(position)=>{
-        const {latitude,longitude}=position.coords;
+
+    const fetchWeatherData=async(latitude,longitude)=>{
         const url=`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_key}`;
+        try{
+            const response=await fetch(url);
+            const weatherData=await response.json();
+            return weatherData;
 
-        fetch(url)
-        .then(res=>res.json())
-        .then(jsonData=>{
-            
-            fetch(`https://openweathermap.org/img/wn/${jsonData.weather[0].icon}@2x.png`)
-            .then(res=>res.blob())
-            .then((result)=>{
-                document.querySelector('.text_location').innerHTML=jsonData.name;
-                document.querySelector('.text_location_country').innerHTML=jsonData.sys.country;
+        }catch(error){
+                console.log('Error fetching weather data:' + error);
+        }
+    
+    }
 
-                document.querySelector('.text_temp').innerHTML=Math.round(jsonData.main.temp);
-                document.querySelector('.text_feelslike').innerHTML=`  ${Math.round(jsonData.main.feels_like)}`;
+    async function fetchWeatherIcon(iconCode){
+        try {
+            const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+            const response = await fetch(iconUrl);
+            const iconBlob = await response.blob();
+            return URL.createObjectURL(iconBlob);
+        } catch (error) {
+            console.error('Error fetching weather icon:', error);
+        }
+    }
 
-                document.querySelector('.text_desc').innerHTML=jsonData.weather[0].description;
+    async function updateUi(data){
+        if(!data)return;
 
-                const imageObjectURL=URL.createObjectURL(result);
-                document.querySelector('#icon').innerHTML=imageObjectURL;
+        document.querySelector('.text_location').textContent = data.name;
+        document.querySelector('.text_location_country').textContent = data.sys.country;
+        document.querySelector('.text_temp').textContent = `${Math.round(data.main.temp)}°C`;
+        document.querySelector('.text_feelslike').textContent = `Feels like: ${Math.round(data.main.feels_like)}°C`;
+        document.querySelector('.text_desc').textContent = data.weather[0].description;
 
-
-            })
-        })
+        const iconURL = await fetchWeatherIcon(data.weather[0].icon);
+        document.querySelector('#icon').src = iconURL;
     }
     
-    navigator.geolocation.getCurrentPosition(geoSuccess)
+    const geoSuccess=async (position)=>{
+        const {latitude,longitude}=position.coords;
+        const fetchWeather= await fetchWeatherData(latitude,longitude);
+        updateUi(fetchWeather);
+    }
+
+
+       
+    
+    navigator.geolocation.getCurrentPosition(
+        geoSuccess,
+        (error)=>console.error('Error getting location'),
+        {enableHighAccuracy:true}    
+    )
 
 }
